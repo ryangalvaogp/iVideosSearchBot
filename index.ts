971ba0e1ada6771b6dd847fce722a42a84764568
Ryan Galvão <60650064+ryangalvaogp@users.noreply.github.com>
2021-04-10 00:15:05 -0300

    feat: add types of data structure

--- index.ts
+++ index.ts
@@ -1,20 +1,36 @@
 import readline from 'readline-sync'
 
-function start (){
-    const content = {}
+interface contentProps {
+    searchTerm?: string
+    prefix?: string
+    sourceContentOriginal?: string
+    sourceContentSanitizes?: string
+    sentences?: [
+        {
+            text: string,
+            keywords: string[],
+            images: string[],
+        }
+    ]
+}
+
+
+
+function start() {
+    const content:contentProps ={} 
 
     content.searchTerm = askAndReturnSearchTerm();
     content.prefix = askAndReturnPrefix();
 
-    function askAndReturnSearchTerm(){
+    function askAndReturnSearchTerm() {
         return readline.question('Escreva um termo para pesquisar no Wikipedia: ')
     };
-    
-    function askAndReturnPrefix(){
+
+    function askAndReturnPrefix() {
         const prefixes = ['Who is', 'What is', 'The history of']
         const selectedPrefixIndex = readline.keyInSelect(prefixes, 'Escolha uma opcao');
         const selectedPrefixText = prefixes[selectedPrefixIndex]
-        
+
         return selectedPrefixText;
     }
     console.log(content)
